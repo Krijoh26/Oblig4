ccd11aa6d2a4a2da498f0935ffb7a0073bbbc98e
Kristine Sjønstå <krijoh26@gmail.com>
2016-04-01 15:01:49 +0200

    Dunno what i have done, some shit works.

--- app/src/main/java/com/example/kristinesjnst/oblig4/MainActivity.java
+++ app/src/main/java/com/example/kristinesjnst/oblig4/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.kristinesjnst.oblig4;
 
+import android.app.ActionBar;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private GoogleMap mMap;
+    private final int[] MAP_TYPES = {GoogleMap.MAP_TYPE_SATELLITE, GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID, GoogleMap.MAP_TYPE_TERRAIN, GoogleMap.MAP_TYPE_NONE};
+    private int currMapTypeIndex = 0;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+
+
+
+        // Getting Map for the SupportMapFragment
+        map = supportMapFragment.getMap();
+        map.setOnMapClickListener(this);
+
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        markerOptions.title(getAddressFromLatLng(latLng));
+
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker());
+        map.addMarker(markerOptions);
+    }
+
+    private String getAddressFromLatLng(LatLng latLng) {
+        Geocoder geocoder= new Geocoder(getApplicationContext(), Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses != null) {
+            return addresses.get(0).getAddressLine(0);
+        }
+        return null;
+    }
+
+//    @Override
+//    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+//        map.setMapType(MAP_TYPES[itemPosition]);
+//
+//        return(true);
+
 }
